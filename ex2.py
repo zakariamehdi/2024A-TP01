3d111ba6519710822d52ad8cca8918b5329dfc30
zakaria <zakarcherr@gmail.com>
2024-09-11 17:43:32 -0400

    finit 3 premier

--- ex2.py
+++ ex2.py
@@ -1,4 +1,9 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+quantite_eau=float(input("Quelle est la quantite d'eau requise?"))
+filtre= 0.2*quantite_eau
+lampes_UV= 0.6*quantite_eau
+chlore= 0.1*quantite_eau
+
+print("Voici les elements necessaires pour", quantite_eau,"L \n     -Filtre =", filtre,"\n     -Lampes UV=", lampes_UV, "\n     -Chlore=", chlore,"kg")
--- ex3.py
+++ ex3.py
@@ -1,5 +1,7 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
-
-speed = ... 
-angle = ...
+import math
+speed =float(input("Quelle est la vitesse initiale?"))
+angle = float(input("Quelle est l'angle de lancee?"))
+distance= (speed**2*(math.sin(2*angle)))/9.81
+print("Distance parcourue =", round(distance,2),"m")
