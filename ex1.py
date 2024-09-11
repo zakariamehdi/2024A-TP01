aabd610102e050777df7b377ece3a7d06dfd27ac
zakaria <zakarcherr@gmail.com>
2024-09-11 17:16:56 -0400

    cest fait

--- ex1.py
+++ ex1.py
@@ -1,8 +1,13 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
-git init, git add, git commit, git push 
+
 
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = input(¨Que est le nom de l'athlète?¨)
-date = input( Quelle est la date du record?)
-sport = input(Dans quel sport?)
+athlete = input("Quel est le nom de l'athlète?")
+date = input("Quelle est la date du record?")
+sport = input("Dans quel sport?")
+categorie = input("Quelle est la categorie specifique?")
+record = input("Quel est le record?")
+a='--------------------'
+chaine1, chaine2, chaine3=f'{date}', f'{sport}-{categorie}', f'{athlete} {country}-{record}'
+print('Nouveau record :\n', a, '\n', chaine1, '\n', chaine2, '\n', chaine3)
