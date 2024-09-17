74fbf69bd3cc2e413818b66152b94ace3b8831ab
zakaria <zakarcherr@gmail.com>
2024-09-17 11:31:09 -0400

    finit exo 5

--- ex5.py
+++ ex5.py
@@ -1,5 +1,8 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
-
-country = ...
-code_medals = ...
+country = input("Quel est le pays?")
+code_medals = input("Quelles medales ont ete remportees?")
+print(country,':')
+print('-',code_medals.count('G'), 'Or')
+print('-',code_medals.count('S'), 'Argent')
+print('-',code_medals.count('B'), 'Bronze')
